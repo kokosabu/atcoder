917f325d7153e0c5f3187d8801a1b497d66ed69f
MIURA Yasuyuki <kokosabu@gmail.com>
2019-05-01 20:11:36 +0900

    ABC007 B - 辞書式順序

---
+++ c/abc007_2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char A[11+1];
+
+    scanf("%s", A);
+
+    if(strcmp(A, "a") == 0) {
+        printf("-1\n");
+    } else {
+        printf("a\n");
+    }
+
+    return 0;
+}
