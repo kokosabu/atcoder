773a9e1c7c5f4ba99b1fd14198a7b58b9fac5302
MIURA Yasuyuki <kokosabu@gmail.com>
2019-05-18 20:39:46 +0900

    ABC014 B - 価格の合計

--- c/abc014_2/main.c
+++ c/abc014_2/main.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-    int n;
-    int X;
-    int a;
-    int sum;
-    int i;
-    int d;
-
-    scanf("%d %d", &n, &X);
-
-    d = 1;
-    for(i = 0; i < n; i++) {
-        scanf("%d", &a);
-        if((X&d) != 0) {
-            sum += a;
-        }
-        d *= 2;
+    int a,n,x,i,ans=0;
+    scanf("%d %d",&n,&x);
+    int b[n];
+    for(i=0;x!=0;i++){
+        b[i]=x%2;
+        x/=2;
     }
-
-    printf("%d\n", sum);
-
+    for(i=0;i<n;i++){
+        scanf("%d",&a);
+        if(b[i]==1) ans+=a;
+    }
+    printf("%d\n",ans);
     return 0;
 }
