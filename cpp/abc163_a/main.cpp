3777922cd3f7d1a047146d8513212c6c28b370ab
MIURA Yasuyuki <kokosabu@gmail.com>
2020-04-19 21:17:33 +0900

    ABC163 A - Circle Pond

---
+++ cpp/abc163_a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <math.h>
+
+using namespace std;
+//#define PI (3.14159265359)
+int main()
+{
+    double R;
+    cin >> R;
+    cout << (2*R*3.14159265359) << endl;
+    return 0;
+}
