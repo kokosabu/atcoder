ac4bfad3f95e5e2ca1694b395a006cef22e6fab5
MIURA Yasuyuki <kokosabu@gmail.com>
2020-04-26 21:44:41 +0900

    ABC164 A - Sheep and Wolves

---
+++ cpp/abc164_a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int S, W;
+    cin >> S >> W;
+    if(W >= S) {
+        cout << "unsafe" << endl;
+    } else {
+        cout << "safe" << endl;
+    }
+    return 0;
+}
