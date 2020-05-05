219a717c873693b2096674e02056a3c1601c9619
MIURA Yasuyuki <kokosabu@gmail.com>
2020-05-05 10:40:32 +0900

    CODE FESTIVAL 2017 qual C A - Can you get AC?

---
+++ cpp/code_festival_2017_qualc_a/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    for(int i = 0; i < S.size()-1; i++) {
+        if(S[i] == 'A' && S[i+1] == 'C') {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
