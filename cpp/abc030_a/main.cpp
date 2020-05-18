9d8456b207a74476dacd7625e458ad66d555ae6a
MIURA Yasuyuki <kokosabu@gmail.com>
2020-05-18 21:42:11 +0900

    ABC030 A - 勝率計算

---
+++ cpp/abc030_a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double A, B, C, D;
+    cin >> A >> B >> C >> D;
+    if((B/A) == (D/C)) {
+        cout << "DRAW" << endl;
+    } else if((B/A) > (D/C)) {
+        cout << "TAKAHASHI" << endl;
+    } else {
+        cout << "AOKI" << endl;
+    }
+    return 0;
+}
