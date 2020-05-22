c96aaebb45b42db2c5b7f175c58e0bb8cf8a3591
MIURA Yasuyuki <kokosabu@gmail.com>
2020-05-22 21:21:32 +0900

    天下一プログラマーコンテスト2015予選A A - 展開図プログラマーコンテスト

---
+++ cpp/tenka1_2015_qualA_a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //cout << 654123 << endl;
+    //cout << 6542 << endl;
+    //cout << 6543 << endl;
+
+    //cout << 6541 << endl;
+    cout << 654231 << endl;
+    return 0;
+}
