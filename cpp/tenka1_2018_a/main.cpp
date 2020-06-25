d0ed07cf09f9c52399cfb2a8cf4186caf064ae7e
MIURA Yasuyuki <kokosabu@gmail.com>
2020-06-25 19:36:01 +0900

    Tenka1 Programmer Beginner Contest A - Measure

---
+++ cpp/tenka1_2018_a/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    if(S.size() == 2) {
+        cout << S << endl;
+    } else {
+        cout << S[2] << S[1] << S[0] << endl;
+    }
+    return 0;
+}
