fe349f67752867eed94b95637417746efe785a90
MIURA Yasuyuki <kokosabu@gmail.com>
2020-06-26 17:44:13 +0900

    ABC109 A - ABC333

---
+++ cpp/abc109_a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    if(A != 2 && B != 2) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
