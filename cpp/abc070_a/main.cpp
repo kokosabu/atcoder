043408c5a1c97295d3f17807df9a98b079199e49
MIURA Yasuyuki <kokosabu@gmail.com>
2020-07-23 08:56:08 +0900

    ABC070 A - Palindromic Number

---
+++ cpp/abc070_a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string N;
+
+    cin >> N;
+
+    if(N[0] == N[2]) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
