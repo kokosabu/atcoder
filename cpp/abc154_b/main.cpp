e8c64ed6d04c42ec09e27c42be8fa5cc8c1dcb23
MIURA Yasuyuki <kokosabu@gmail.com>
2020-08-13 09:12:34 +0900

    ABC154 B - I miss you...

---
+++ cpp/abc154_b/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    for(int i = 0; i < S.size(); i++) {
+        cout << "x";
+    }
+    cout << endl;
+    return 0;
+}
