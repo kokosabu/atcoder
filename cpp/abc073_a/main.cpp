cf079b5bfd030658910c3c8b5ac45dc6ddc8680e
MIURA Yasuyuki <kokosabu@gmail.com>
2020-08-21 01:43:56 +0900

    ABC073 A - September 9

---
+++ cpp/abc073_a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string N;
+    cin >> N;
+
+    if(N[0] == '9' || N[1] == '9') {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
