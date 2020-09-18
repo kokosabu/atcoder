f88c44a754a753a49998013d273fb4207dfc1065
MIURA Yasuyuki <kokosabu@gmail.com>
2020-09-19 00:36:45 +0900

    ARC016 A - クイズゲーム

---
+++ cpp/arc016_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    if(M != 1) {
+        cout << 1 << endl;
+    } else {
+        cout << 2 << endl;
+    }
+    return 0;
+}
