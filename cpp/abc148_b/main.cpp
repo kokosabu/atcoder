1396e8ff1d0e6e483dba4a56530ede352bfbc731
MIURA Yasuyuki <kokosabu@gmail.com>
2020-09-30 23:46:17 +0900

    ABC148 B - Strings with the Same Length

---
+++ cpp/abc148_b/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    string S, T;
+    cin >> S >> T;
+    for(int i = 0; i < N; i++) {
+        cout << S[i] << T[i];
+    }
+    cout << endl;
+    return 0;
+}
