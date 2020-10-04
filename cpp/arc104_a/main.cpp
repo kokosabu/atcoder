927e95dfaf13a7f6824bbcad126661c253c93cc9
MIURA Yasuyuki <kokosabu@gmail.com>
2020-10-04 20:51:32 +0900

    ARC104 A - Plus Minus

---
+++ cpp/arc104_a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    int X = (A+B) / 2;
+    int Y = (A-B) / 2;
+    cout << X << " " << Y << endl;
+    return 0;
+}
