d51d09a71256d7a3ef5b863c32400da65f4cbc62
MIURA Yasuyuki <kokosabu@gmail.com>
2020-11-05 19:56:36 +0900

    ABC093 A - abc of ABC

---
+++ cpp/abc093_a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    sort(S.begin(), S.end());
+    if(S == "abc") {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
