44e77675d4eb0feb466fc5560c03eb01b64b93a7
MIURA Yasuyuki <kokosabu@gmail.com>
2020-11-12 21:22:03 +0900

    ABC134 B - Golden Apple

---
+++ cpp/abc134_b/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, D;
+    cin >> N >> D;
+    if((N%(2*D+1)) == 0) {
+        cout << N/(2*D+1) << endl;
+    } else {
+        cout << N/(2*D+1)+1 << endl;
+    }
+    return 0;
+}
