df453a319d3d37cdc0fc71196563919abb21f9d1
MIURA Yasuyuki <kokosabu@gmail.com>
2020-11-20 21:58:19 +0900

    ABC090 A - Diagonal String

---
+++ cpp/abc090_a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i < 3; i++) {
+        string c;
+        cin >> c;
+        cout << c[i];
+    }
+    cout << endl;
+    return 0;
+}
