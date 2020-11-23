1260d77a232989a30d348ee59643d606168f63da
MIURA Yasuyuki <kokosabu@gmail.com>
2020-11-24 00:58:57 +0900

    ARC002 A - うるう年

---
+++ arc002_1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Y;
+    cin >> Y;
+    if(Y%400 == 0) {
+        cout << "YES" << endl;
+    } else if(Y%100 == 0) {
+        cout << "NO" << endl;
+    } else if(Y%4 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
