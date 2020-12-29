ebf988f68ba5781935789ac4f078408894218fed
MIURA Yasuyuki <kokosabu@gmail.com>
2020-12-29 18:02:30 +0900

    ABC079 A - Good Integer

---
+++ cpp/abc079_a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string N;
+    cin >> N;
+    if(N[0] == N[1] && N[1] == N[2]) {
+        cout << "Yes" << endl;
+    } else if(N[1] == N[2] && N[2] == N[3]) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
