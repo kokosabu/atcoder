103a80f62ae37ae73cd688de0ec8605a5b0f8c94
MIURA Yasuyuki <kokosabu@gmail.com>
2021-01-04 20:53:54 +0900

    ABC077 B - Around Square

---
+++ abc077_b/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int answer = (int)pow((int)sqrt(N), 2);
+    cout << answer << endl;
+    return 0;
+}
