c85fa28a286f0acc99c14e2d05ccb75c80e1e322
MIURA Yasuyuki <kokosabu@gmail.com>
2021-01-07 00:10:35 +0900

    ABC159 C - Maximum Volume

---
+++ cpp/abc159_c/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double L;
+    cin >> L;
+    cout << fixed << setprecision(7) << (L/3)*(L/3)*(L/3) << endl;
+    return 0;
+}
