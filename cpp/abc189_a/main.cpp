5874579bd944093ba81e96315694e43171d24b7e
MIURA Yasuyuki <kokosabu@gmail.com>
2021-01-24 10:33:02 +0900

    ABC189 A - Slot

---
+++ cpp/abc189_a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string C;
+    cin >> C;
+    if(C[0] == C[1] && C[1] == C[2]) {
+        cout << "Won" << endl;
+    } else {
+        cout << "Lost" << endl;
+    }
+    return 0;
+}
