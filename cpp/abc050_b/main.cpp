046e4a7290a9407a43899d35e8306436fb753005
MIURA Yasuyuki <kokosabu@gmail.com>
2021-04-07 23:51:01 +0900

    ABC050 B - Contest with Drinks Easy

---
+++ cpp/abc050_b/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+  vector<int> T(N);
+  int sum = 0;
+  for(int i = 0; i < N; i++) {
+    cin >> T[i];
+    sum += T[i];
+  }
+  int M;
+  cin >> M;
+  vector<int> P(M);
+  vector<int> X(M);
+  for(int i = 0; i < M; i++) {
+    cin >> P[i] >> X[i];
+  }
+
+  for(int i = 0; i < M; i++) {
+    cout << sum - T[P[i]-1] + X[i] << endl;
+  }
+
+  return 0;
+}
