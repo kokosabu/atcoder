8cd8662b9783a23ca693eae8285dd562200dc748
MIURA Yasuyuki <kokosabu@gmail.com>
2024-01-18 23:56:46 +0900

    abc292 a

---
+++ cpp/abc292_a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    transform(S.begin(), S.end(), S.begin(), ::toupper);
+    cout << S << endl;
+    return 0;
+}
